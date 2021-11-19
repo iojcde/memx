6e93394d3993f567bd42a05fc7e50e8622a066ad
JcdeA <io@fosshost.org>
2021-11-19 03:05:37 +0000

    tweak bg opacity

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const Nav: React.FC = () => {
   return (
-    <nav className="sticky top-0 z-30 flex justify-center items-center w-full px-4 py-4 my-0 mb-8 backdrop-filter backdrop-blur-lg bg-opacity-0 firefox:bg-opacity-90 dark:bg-[#111827] dark:firefox:bg-opacity-90 bg-white border-b border-primary">
+    <nav className="sticky top-0 z-30 flex justify-center items-center w-full px-4 py-4 my-0 mb-8 backdrop-filter backdrop-blur-lg bg-opacity-0 firefox:bg-opacity-90 dark:bg-[#111827] dark:firefox:bg-opacity-[85] bg-white border-b border-primary">
       <div className="flex justify-between items-center max-w-2xl w-full">
         <div className="text-primary font-semibold dark:text-gray-100 text-black">
           <Link href="/">Jeeho Ahn</Link>
--- components/ViewCounter.tsx
+++ components/ViewCounter.tsx
@@ -4,20 +4,26 @@ import useSWR from 'swr'
 import fetcher from 'lib/fetcher'
 import { Views } from 'lib/types'
 
-const ViewCounter: React.FC<{ slug: string, text?: string }> = ({ slug, text }) => {
-    const { data } = useSWR<Views>(`/api/views/${slug}`, fetcher)
-    const views = new Number(data?.total)
+const ViewCounter: React.FC<{ slug: string; text?: string }> = ({
+  slug,
+  text,
+}) => {
+  const { data } = useSWR<Views>(`/api/views/${slug}`, fetcher)
+  const views = new Number(data?.total)
 
-    useEffect(() => {
-        const registerView = () =>
-            fetch(`/api/views/${slug}`, {
-                method: `POST`,
-            })
-        if (process.env.NODE_ENV === `production`) {
-            registerView()
-        }
-    }, [slug])
-    return <span>{`${views > 0 ? views.toLocaleString() : `–––`}  ${text || 'views'}`}</span>
-
+  useEffect(() => {
+    const registerView = () =>
+      fetch(`/api/views/${slug}`, {
+        method: `POST`,
+      })
+    if (process.env.NODE_ENV === `production`) {
+      registerView()
+    }
+  }, [slug])
+  return (
+    <span>{`${views > 0 ? views.toLocaleString() : `–––`}  ${
+      text || `views`
+    }`}</span>
+  )
 }
 export default ViewCounter
