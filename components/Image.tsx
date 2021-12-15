a682074b3e4894c8dd05c65fb6d6eb134783a6dc
Jeeho Ahn <io@fosshost.org>
2021-12-16 01:54:19 +0900

    Update Image.tsx

--- components/Image.tsx
+++ components/Image.tsx
@@ -2,13 +2,13 @@ import NextImage, { ImageProps } from 'next/image'
 import React from 'react'
 
 const myLoader = ({ src, width, quality }) => {
-  return `https://jcdea-website.vercel.app/_next/image/${src}?w=${width}&q=${
+  return `https://jcdea-website.vercel.app/_next/image${src}?w=${width}&q=${
     quality || 75
   }`
 }
 
 const Image: React.FC<ImageProps> = (props) => {
-  if (process.env.CLOUDFLARE == `true`) {
+  if (process.env.CLOUDFLARE) {
     return <NextImage loader={myLoader} {...props} />
   } else {
     return <NextImage {...props} />
