f8c3e0710b94452423c461eeb3db4f8837dc85d2
Jeeho Ahn <io@fosshost.org>
2021-12-16 01:56:36 +0900

    Update Image.tsx

--- components/Image.tsx
+++ components/Image.tsx
@@ -2,7 +2,7 @@ import NextImage, { ImageProps } from 'next/image'
 import React from 'react'
 
 const myLoader = ({ src, width, quality }) => {
-  return `https://jcdea-website.vercel.app/_next/image${src}?w=${width}&q=${
+  return `https://jcdea-website.vercel.app/_next/image?url=${src}?w=${width}&q=${
     quality || 75
   }`
 }
