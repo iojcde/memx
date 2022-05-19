56a7ee3f3be46d252a33ee28db0182aa6ad24021
JcdeA <io@fosshost.org>
2022-05-19 22:41:41 +0000

    feat: reduce payload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import '../styles/fonts.css'
 import { ThemeProvider } from 'next-themes'
 import { AppProps } from 'next/app'
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -66,7 +66,7 @@ const Home: NextPage = () => {
   return (
     <Layout hero>
       <div className="h-[90vh]">
-        <div className="z-10 float-right flex h-[90vh] dark:lg:border dark:border-gray-400 w-full max-w-[78rem] bg-rose-200 transition duration-100 dark:bg-black">
+        <div className="z-10 float-right flex h-[90vh] w-full max-w-[78rem] bg-rose-200 transition duration-100 dark:border-gray-400 dark:bg-black dark:lg:border">
           <div className="z-20 mt-10 px-4 pb-8 md:mt-36 lg:mt-40 lg:px-12">
             <h1 className="text-[18vw] text-6xl leading-tight dark:text-white sm:text-7xl lg:text-8xl">
               <b> I&apos;m a full-stack developer & designer based in Seoul.</b>
@@ -198,6 +198,7 @@ const Home: NextPage = () => {
                   src={v}
                   alt=""
                   className="max-h-96 rounded border"
+                  loading="lazy"
                 />
               ))}
             </div>
