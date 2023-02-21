e828ec7088205946f0241daa8a08797d6aea5590
Jeeho Ahn <io@fosshost.org>
2023-02-22 08:46:12 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,9 @@
+import '../styles/globals.css'
+
+import '../styles/cursor.css'
+
+import '../styles/syntax.css'
+
 export default function RootLayout({
   // Layouts must accept a children prop.
   // This will be populated with nested layouts or pages
