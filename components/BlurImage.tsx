d7015e8c464c9c061e017429ce1a7e32f00bbd22
Jeeho Ahn <io@fosshost.org>
2023-02-23 08:57:59 +0900

    Update BlurImage.tsx

--- components/BlurImage.tsx
+++ components/BlurImage.tsx
@@ -6,7 +6,6 @@ const Image = (props: ImageProps) => {
   return (
     <div className={` inline-flex overflow-hidden`}>
       <NextImage
-        src={props.src}
         blurDataURL={!isStatic && (props.src as string)}
         placeholder={props.width > 40 ? `blur` : `empty`}
         {...props}
