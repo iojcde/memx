86bba96401a4e635b42b4205e972ca67c2f4e631
Jeeho Ahn <io@fosshost.org>
2023-02-26 20:22:12 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const { withContentlayer } = require('next-contentlayer')
 const fs = require('fs')
-<<<<<<< HEAD
-const filenamesFile = fs.readFileSync('./data/filenames.json', 'utf8')
-=======
 const filenames = fs.readFileSync('./data/filenames.json', 'utf8')
->>>>>>> c4d4667 (cool post heaader)
 
 const filenames = JSON.parse(filenamesFile)
 module.exports = withContentlayer({
@@ -14,11 +10,7 @@ module.exports = withContentlayer({
   },
   reactStrictMode: true,
   redirects: async () => {
-<<<<<<< HEAD
-    return Object.keys(filenames).map((key) => ({
-=======
     return Object.keys(JSON.parse(filenames)).map((key) => ({
->>>>>>> c4d4667 (cool post heaader)
       source: `/${key}`,
       destination: `/${filenames[key]}`,
       permanent: false,
