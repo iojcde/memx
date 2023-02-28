c790367b1c93f993bef3d5a358d1a80917acd2f6
Jeeho Ahn <io@fosshost.org>
2023-02-28 01:48:54 +0000

    fix types 2

--- app/[slug]/layout.tsx
+++ app/[slug]/layout.tsx
@@ -1,5 +1,5 @@
 import { Sidebar } from './Sidebar'
-import { allDocuments } from 'contentlayer/generated'
+import { buildResearchTree } from 'utils/buildTree'
 
 const Layout = ({ children }) => (
   <div className="flex">
@@ -8,17 +8,7 @@ const Layout = ({ children }) => (
       className="thin-scrollbar sticky top-16 hidden shrink-0 border-r border-neutral-200 dark:border-neutral-800 lg:block"
     >
       <div className=" -ml-3 h-full overflow-y-scroll p-8 pl-16">
-        <Sidebar
-          tree={allDocuments.map((doc) => ({
-            title: doc.title,
-            nav_title: doc.title,
-            collapsible: false,
-            collapsed: false,
-            label: ``,
-            urlPath: `/${doc.hex}`,
-            children: [],
-          }))}
-        />
+        <Sidebar tree={buildResearchTree()} />
       </div>
       {/* <div className="dark:from-neutral-950/0 dark:to-neutral-950/100 absolute inset-x-0 top-0 h-8 bg-gradient-to-t from-white/0 to-white/100" />
       <div className="dark:from-neutral-950/0 dark:to-neutral-950/100 absolute inset-x-0 bottom-0 h-8 bg-gradient-to-b from-white/0 to-white/100" /> */}
