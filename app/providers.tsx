84250fb73e040f61e5dcf28f742e1d98bdfe24d6
Jeeho Ahn <io@jcde.xyz>
2024-06-18 12:14:09 +0900

    cool

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,12 +1,14 @@
 'use client'
 
-import { SearchProvider } from 'components/SearchContext'
+// import { SearchProvider } from 'components/SearchContext'
 import { ThemeProvider } from 'next-themes'
 
 export function Providers({ children }) {
   return (
     <ThemeProvider attribute="class">
-      <SearchProvider>{children}</SearchProvider>
+      {/* <SearchProvider> */}
+      {children}
+      {/* </SearchProvider> */}
     </ThemeProvider>
   )
 }
