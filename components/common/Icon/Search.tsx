23e9cc8072375a2f9996bffe6a9465930d2269fb
Jeeho Ahn <io@jcde.xyz>
2024-06-20 14:56:08 +0000

    yeah

--- components/common/Icon/Search.tsx
+++ components/common/Icon/Search.tsx
@@ -1,6 +1,6 @@
 import { FC, SVGProps } from 'react'
 
-export const SearchIcon: FC<SVGProps> = (props: SVGProps) => {
+export const SearchIcon: FC<SVGProps<SVGSVGElement>> = (props) => {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
